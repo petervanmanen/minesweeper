9c67d0292335e2352db3d0b029113ede54c1d3ca
Peter van Manen <peter@salt.dev>
2023-04-13 21:41:38 +0200

    fixed state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Board from './Board';
 import { Game } from './Game';
@@ -8,10 +8,11 @@ import { Game } from './Game';
 function App() {
   const [, updateState] = React.useState<any>();
   const forceUpdate = React.useCallback(() => updateState({}), []);
-  //const [game, setGame] = useState();
+  const [game, setGame] = useState(new Game(25, 25, 50));
   
-  let game = new Game(25, 25, 50);
+  //let game = new Game(25, 25, 50);
   game.setGameCallBack((game: Game) => {
+    setGame(game);
     forceUpdate();
   });
   document.addEventListener("contextmenu", (event) => {
